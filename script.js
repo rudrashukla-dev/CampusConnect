38b627569a3d2053c5d882ba29cd1c9f483544cf
Rudra Shukla <rudrashukla837@gmail.com>
2026-09-23 22:04:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-const favMovie = "Avatar";
-let guess = prompt("Guess your favrioute movie");
-
-if (guess == favMovie) {
-    console.log("congratulation you guessed correctly " + favMovie);
-}
-else {
-    while ((guess != favMovie) && (guess != "quit")) {
-        console.log("wrong");
-        guess = prompt("Retry");
-    }
-}
+let name;
 
 
